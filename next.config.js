b53462962a1ee3fec1591e9e2258b3220ee72b7f
sora-web <brave27yuto@gmail.com>
2022-09-04 13:56:16 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
